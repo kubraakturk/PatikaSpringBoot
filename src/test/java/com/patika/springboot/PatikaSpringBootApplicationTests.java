e053d4eaf2e5a10c335c8748352e142e03197721
Kübra Aktürk <kubraakturk1@gmail.com>
2022-10-12 18:32:38 +0300

    Initial commit

---
+++ src/test/java/com/patika/springboot/PatikaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patika.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatikaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
